9547d8b69664825fa476c61258d74c4b6d834875
Your Name <deudajm@gmail.com>
2024-12-29 20:31:27 +0800

    `Added modal for adding manga with form and buttons`

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { manga, getManga, options, data } from "./store";
 import { Icon } from "@iconify/vue";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, Transition } from "vue";
 import { Bar } from "vue-chartjs";
 import {
   getFirestore,
@@ -50,6 +50,11 @@ const add = async () => {
 onMounted(() => {
   getManga();
 });
+
+const showModal = ref(false);
+const addManga = () => {
+  showModal.value = !showModal.value;
+};
 </script>
 
 <template>
@@ -57,59 +62,19 @@ onMounted(() => {
     class="flex justify-center items-center h-screen bg-gray-100 text-gray-800"
   >
     <div class="border p-5 w-full">
-      <h1 class="font-semibold text-lg my-2">Manga Lists</h1>
+      <h1 class="font-bold text-lg my-2">Manga Lists</h1>
+
       <div class="flex justify-center items-center">
         <div class="size-52">
           <Pie :data="data" :options="options" />
         </div>
       </div>
-      <!-- add manga -->
-      <div class="grid sm:grid-cols-2 grid-cols-1 gap-2">
-        <div class="my-1">
-          <p class="text-xs font-semibold">Name</p>
-          <input
-            type="text"
-            v-model="name"
-            class="border p-1 text-xs w-full outline-none bg-transparent"
-          />
-        </div>
-        <div class="my-1">
-          <p class="text-xs font-semibold">Chapter</p>
-          <input
-            v-model="chapter"
-            type="text"
-            class="border p-1 text-xs w-full outline-none bg-transparent"
-          />
-        </div>
-        <div class="my-1">
-          <p class="text-xs font-semibold">Status</p>
-          <input
-            type="text"
-            v-model="status"
-            class="border p-1 text-xs w-full outline-none bg-transparent"
-          />
-        </div>
-        <div class="my-1">
-          <p class="text-xs font-semibold">Rating</p>
-          <input
-            type="text"
-            v-model="rating"
-            class="border p-1 text-xs w-full outline-none bg-transparent"
-          />
-        </div>
-      </div>
-      <div class="flex justify-start w-full items-center gap-2 my-2">
+      <div>
         <button
-          @click="clear"
-          class="w-32 border bg-gray-300 font-semibold text-sm py-2"
-        >
-          Clear
-        </button>
-        <button
-          @click="add"
+          @click="addManga"
           class="w-32 bg-gray-800 text-white font-semibold text-sm py-2"
         >
-          Add
+          Add Manga
         </button>
       </div>
 
@@ -162,6 +127,87 @@ onMounted(() => {
       </div>
     </div>
   </div>
+
+  <!-- modal -->
+  <transition>
+    <div
+      v-if="showModal"
+      class="fixed inset-0 z-50 bg-gray-800/30 backdrop-blur"
+    >
+      <div
+        class="relative flex justify-center items-center h-full text-gray-800"
+      >
+        <div class="relative bg-gray-200 border p-5">
+          <form action="" @submit.prevent="add">
+            <!-- add manga -->
+            <div class="flex justify-between items-center mb-2">
+              <p class="text-lg font-extrabold">Enter Manga Data</p>
+              <div @click="addManga">
+                <Icon
+                  icon="material-symbols-light:close"
+                  width="24"
+                  height="24"
+                />
+              </div>
+            </div>
+
+            <div class="grid sm:grid-cols-2 grid-cols-1 gap-2">
+              <div class="my-1">
+                <p class="text-xs font-semibold">Name</p>
+                <input
+                  type="text"
+                  v-model="name"
+                  required
+                  class="border border-gray-700/50 p-1 text-xs w-full outline-none bg-transparent"
+                />
+              </div>
+              <div class="my-1">
+                <p class="text-xs font-semibold">Chapter</p>
+                <input
+                  v-model="chapter"
+                  type="text"
+                  required
+                  class="border p-1 border-gray-700/50 text-xs w-full outline-none bg-transparent"
+                />
+              </div>
+              <div class="my-1">
+                <p class="text-xs font-semibold">Status</p>
+                <input
+                  type="text"
+                  v-model="status"
+                  required
+                  class="border p-1 border-gray-700/50 text-xs w-full outline-none bg-transparent"
+                />
+              </div>
+              <div class="my-1">
+                <p class="text-xs font-semibold">Rating</p>
+                <input
+                  type="text"
+                  v-model="rating"
+                  required
+                  class="border p-1 border-gray-700/50 text-xs w-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+            <div class="flex justify-start w-full items-center gap-2 my-2">
+              <button
+                @click="clear"
+                class="w-32 border bg-gray-300 font-semibold text-sm py-2"
+              >
+                Clear
+              </button>
+              <button
+                type="submit"
+                class="w-32 bg-gray-800 text-white font-semibold text-sm py-2"
+              >
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <style>
@@ -169,5 +215,14 @@ onMounted(() => {
 :root {
   font-family: "Manrope", serif;
   font-optical-sizing: auto;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
