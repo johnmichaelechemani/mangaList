156564940ab10689ce3e3953cb0e56c723f73465
Your Name <deudajm@gmail.com>
2025-01-17 21:07:35 +0800

    `Added Pagination component and removed manual pagination implementation`

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ import listCard from "./components/listCard.vue";
 import ChartLoading from "./components/ChartLoading.vue";
 import InfoLoading from "./components/InfoLoading.vue";
 import ListLoading from "./components/ListLoading.vue";
+import Pagination from "./components/pagination.vue";
 
 const db = getFirestore();
 const showModal = ref(false);
@@ -288,35 +289,7 @@ const addManga = () => {
       </div>
 
       <!-- pagination -->
-
-      <div class="flex justify-between gap-2 mb-2">
-        <select class="text-xs border w-full rounded-full px-2 font-semibold">
-          <option value="">All</option>
-          <option value="reading">10</option>
-          <option value="ongoing">25</option>
-          <option value="dropped">50</option>
-        </select>
-        <div class="flex justify-center items-center gap-2">
-          <button class="p-1 border rounded-full bg-gray-400/5">
-            <Icon
-              icon="material-symbols-light:arrow-back-2-rounded"
-              width="20"
-              height="20"
-            />
-          </button>
-          <div class="px-2 border rounded-full py-1 text-xs font-semibold">
-            2/3
-          </div>
-          <button class="p-1 border rounded-full bg-gray-400/5">
-            <Icon
-              icon="material-symbols-light:play-arrow-rounded"
-              width="20"
-              height="20"
-            />
-          </button>
-        </div>
-      </div>
-
+      <Pagination />
       <!--  -->
 
       <div v-if="!isMangaLoading">
