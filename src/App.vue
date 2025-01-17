4fe823918735739cb3717ee187ed5e42ee04e198
Your Name <deudajm@gmail.com>
2025-01-17 21:04:53 +0800

    `Added pagination controls to App.vue`

--- src/App.vue
+++ src/App.vue
@@ -286,6 +286,39 @@ const addManga = () => {
           </button>
         </div>
       </div>
+
+      <!-- pagination -->
+
+      <div class="flex justify-between gap-2 mb-2">
+        <select class="text-xs border w-full rounded-full px-2 font-semibold">
+          <option value="">All</option>
+          <option value="reading">10</option>
+          <option value="ongoing">25</option>
+          <option value="dropped">50</option>
+        </select>
+        <div class="flex justify-center items-center gap-2">
+          <button class="p-1 border rounded-full bg-gray-400/5">
+            <Icon
+              icon="material-symbols-light:arrow-back-2-rounded"
+              width="20"
+              height="20"
+            />
+          </button>
+          <div class="px-2 border rounded-full py-1 text-xs font-semibold">
+            2/3
+          </div>
+          <button class="p-1 border rounded-full bg-gray-400/5">
+            <Icon
+              icon="material-symbols-light:play-arrow-rounded"
+              width="20"
+              height="20"
+            />
+          </button>
+        </div>
+      </div>
+
+      <!--  -->
+
       <div v-if="!isMangaLoading">
         <template v-if="filteredAndSortedManga.length > 0">
           <listCard :manga="filteredAndSortedManga" />
